c6d810002291a84cb3696eab636d74ecefaf19ae
m0b1 <mail@m0b1.dev>
2024-03-10 12:58:39 -0400

    Refactoring controller mapping to address Sonar concern.

--- code/src/main/java/dev/m0b1/uishock/App.java
+++ code/src/main/java/dev/m0b1/uishock/App.java
@@ -2,14 +2,14 @@ package dev.m0b1.uishock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
 public class App {
 
-	@RequestMapping("/")
+	@GetMapping("/")
     public String getGreeting() {
         return "Hello World!";
     }
